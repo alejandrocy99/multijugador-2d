2152d9fca82e22fbd139e4f0271b2dc0efb84f07
alejandrocy99 <moflycy@gmail.com>
2025-01-22 12:20:04 +0100

    Agregar archivos de configuración y metadatos para TextMesh Pro y Unity

---
+++ Assets/ConnectionManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConectionManager : MonoBehaviourPunCallbacks
+{
+
+    public TextMeshProUGUI conexion;
+    public Button connectButton;
+    private void Start()
+    {
+        
+        connectButton.gameObject.SetActive(false);
+        // Start the connection
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    override
+    public void OnConnectedToMaster()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            conexion.text = "Conectado";
+            connectButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            conexion.text = "No se pudo conectar al servidor";
+        }
+    
+    }
+
+    public void ButtonConnect(){
+        RoomOptions options = new RoomOptions() {MaxPlayers = 4};
+        PhotonNetwork.JoinOrCreateRoom("room1", options, TypedLobby.Default);
+        
+    }
+    override
+    public void OnJoinedRoom()
+    {
+        Debug.Log("Joined room" + PhotonNetwork.CurrentRoom.Name);
+        conexion.text = "Conectado a la sala " + PhotonNetwork.CurrentRoom.Name;
+    }
+}
