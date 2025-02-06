4c3b57f7029ffd125d6240b33a0236153f092e78
alejandrocy99 <moflycy@gmail.com>
2025-02-06 10:03:56 +0100

    Agregar clase Player con lógica de movimiento y salto

---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float velocity, jumpForce;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        rb.velocity = (transform.right * velocity * Input.GetAxis("Horizontal")) + (transform.up * rb.velocity.y);
+        if (rb.velocity.x > 0.1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (rb.velocity.x < -0.1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
